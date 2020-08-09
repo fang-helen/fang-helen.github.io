359271076c7dd8324ed1bbe36d2b18cfecc1f8c6
fang-helen <helenfang524@gmail.com>
2020-08-09 01:53:24 -0500

    Change background color on scroll

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+const startColor = 444444;
+
 function load() {
   window.addEventListener('scroll', function() {
-      document.getElementById('showScroll').innerHTML = window.pageYOffset + 'px';
+      const offset = window.pageYOffset;
+      document.getElementById('showScroll').innerHTML = offset + 'px';
+
+      const limit = document.body.style.height - window.innerHeight;
+      const displace = Math.floor(44 * offset/limit);
+      const result = displace + 100 * displace + 10000 * displace + startColor;
+      document.body.style.backgroundColor = "#" + result;
     });
   const rect = document.getElementById('showTop').getBoundingClientRect();
   var left = rect.left + window.scrollX;
