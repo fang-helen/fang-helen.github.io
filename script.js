c3ace91dde222483ffed8f09e2c2cd58e8035488
fang-helen <helenfang524@gmail.com>
2020-08-09 01:29:55 -0500

    Test js functionality

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ function load() {
   window.addEventListener('scroll', function() {
       document.getElementById('showScroll').innerHTML = window.pageYOffset + 'px';
     });
+  const rect = document.getElementById('showTop').getBoundingClientRect();
+  var left = rect.left + window.scrollX;
+  var top = rect.top + window.scrollY;
+
+  document.getElementById('showTop').innerText = top;
 }
     
