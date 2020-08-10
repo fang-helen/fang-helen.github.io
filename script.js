90f025aeb2e48bdce3907ceed7d11e7f003c4ef7
fang-helen <helenfang524@gmail.com>
2020-08-09 20:56:46 -0500

    Add transition

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
 const color = "020c15";
 
 function load() {
+  // trigger animation
+  document.getElementById('intro-content').className = 'after-load';
+
   window.addEventListener('scroll', function() {
-      // scroll position
-      const offset = window.pageYOffset;
-
-      // update color while scrolling
-      const limit = document.body.style.height - window.innerHeight;
-      const startColor = parseInt(color, 16);
-      var b = startColor & 0xFF,
-          g = (startColor & 0xFF00) >>> 8,
-          r = (startColor & 0xFF0000) >>> 16;
-      const delta = offset/limit;
-      b = Math.floor(b * delta) + b;
-      g += Math.floor(g * delta) + g;
-      r += Math.floor(r * delta) + r;
-      
-      const result = "rgb(" + [r, g, b].join(",") + ")";
-      document.body.style.backgroundColor = result;
-
-      document.getElementById('showScroll').innerHTML = offset + 'px  ' + result;
+      scrollActions();
     });
 
   // get position of element
@@ -28,4 +14,25 @@ function load() {
   var top = rect.top + window.scrollY;
 
   document.getElementById('showTop').innerText = top;
-} 
+}
+
+function scrollActions() {
+  // scroll position
+  const offset = window.pageYOffset;
+
+  // update color while scrolling
+  const limit = document.body.style.height - window.innerHeight;
+  const startColor = parseInt(color, 16);
+  var b = startColor & 0xFF,
+      g = (startColor & 0xFF00) >>> 8,
+      r = (startColor & 0xFF0000) >>> 16;
+  const delta = offset/limit;
+  b = Math.floor(b * delta) + b;
+  g += Math.floor(g * delta) + g;
+  r += Math.floor(r * delta) + r;
+  
+  const result = "rgb(" + [r, g, b].join(",") + ")";
+  document.body.style.backgroundColor = result;
+
+  document.getElementById('showScroll').innerHTML = offset + 'px  ' + result;
+}
