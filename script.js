d118ca0d382740c641353bff1433306bc196e1de
fang-helen <helenfang524@gmail.com>
2020-08-10 00:10:05 -0500

    Refactor scroll loading

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const color = "020c15";
 
+/** Actions to take when the page loads. */
 function load() {
-  // trigger animation
+  // trigger animation of intro section
   document.getElementById('intro-content').classList.add('after-load');
 
   window.addEventListener('scroll', function() {
       scrollActions();
     });
 
-  // get position of element
+  // get position of element (test)
   const rect = document.getElementById('showTop').getBoundingClientRect();
   var left = rect.left + window.scrollX;
   var top = rect.top + window.scrollY;
@@ -16,15 +17,12 @@ function load() {
   document.getElementById('showTop').innerText = top;
 }
 
+/** Eventlistener function that triggers when user scrolls. */
 function scrollActions() {
   // scroll position
   const offset = window.pageYOffset;
 
-  // show sections if needed
-  const aboutmeThreshold = document.getElementById('aboutme-content').clientHeight;
-  if(offset >= aboutmeThreshold * .1) {
-    document.getElementById('aboutme-content').classList.add('after-load');
-  }
+  checkLoadElement('aboutme-content');
 
   // update color while scrolling
   const limit = document.body.style.height - window.innerHeight;
@@ -38,7 +36,23 @@ function scrollActions() {
   r += Math.floor(r * delta) + r;
   
   const result = "rgb(" + [r, g, b].join(",") + ")";
-  document.body.style.backgroundColor = result;
+  // document.body.style.backgroundColor = result;
 
   document.getElementById('showScroll').innerHTML = offset + 'px  ' + result;
+
+
+  const rect = document.getElementById('showTop').getBoundingClientRect();
+  console.log(rect.top);
+}
+
+/** 
+ * Check to see if it is time to load an element.
+ * 
+ * @param {string} elemName The id of the element in the document.
+ */
+function checkLoadElement(elemName) {
+  const rect = document.getElementById(elemName).getBoundingClientRect();
+  if(window.innerHeight - rect.top > window.innerHeight * .1) {
+    document.getElementById(elemName).classList.add('after-load');
+  }
 }
