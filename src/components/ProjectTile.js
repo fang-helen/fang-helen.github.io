1395af2f3c9bd6ea4ac20d0425a4396278da001d
fang-helen <helen524@gmail.com>
2021-12-27 15:17:09 -0600

    add files

---
+++ src/components/ProjectTile.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./ProjectTile.css";
+
+class ProjectTile extends React.Component {
+  render() {
+    return (
+      <div class="project-tile-square">
+        <div class="project-tile"></div>
+      </div>
+    );
+  }
+}
+
+export default ProjectTile;
--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -1,7 +1,22 @@
 import Section from "../components/Section";
+import ProjectTile from "../components/ProjectTile";
+
+import "./Projects.css";
 
 function Projects() {
-  return <Section elementId="projects-section" title="My Projects" />;
+  return (
+    <Section
+      elementId="projects-section"
+      title="My Projects"
+      children={
+        <div id="projects-container">
+          <ProjectTile />
+          <ProjectTile />
+          <ProjectTile />
+        </div>
+      }
+    />
+  );
 }
 
 export default Projects;
